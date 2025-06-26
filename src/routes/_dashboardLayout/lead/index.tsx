9a85f793269b48dad82f3b2bc9890e7cfe93ffb2
Tushar Dutta <tz0540245@gmail.com>
2025-06-26 07:30:13 +0530

    leads list ui changes

--- src/routes/_dashboardLayout/lead/index.tsx
+++ src/routes/_dashboardLayout/lead/index.tsx
@@ -6,28 +6,30 @@ export const Route = createFileRoute("/_dashboardLayout/lead/")({
 
 function RouteComponent() {
   return (
-    <div className="w-full">
+    <div className="w-[1000px] mx-auto">
       {" "}
       <div className="overflow-x-auto">
         {" "}
-        <div className="mt-10 inline-flex space-x-2">
+        <div className="mt-10 inline-flex ">
           {" "}
           {[...Array(20)].map((_, i) => (
             <div
               key={i}
-              className="flex-shrink-0 w-[300px] rounded-lg border shadow bg-white"
+              className="flex-shrink-0 w-[300px] rounded-lg bg-transparent "
             >
               {/* Header */}
-              <div className="bg-purple-600 text-white px-4 py-2 rounded-t-lg">
-                <h3 className="font-semibold">Column {i + 1}</h3>
+              <div className="  px-4 rounded-t-lg rounded-md">
+                <h3 className="font-semibold py-2 px-3 bg-purple-600 text-white">
+                  Column {i + 1}
+                </h3>
               </div>
 
               {/* Body - list of cards */}
-              <div className="p-2 space-y-2 max-h-[500px] overflow-y-auto">
+              <div className=" px-4 space-y-2 max-h-[500px] overflow-y-auto">
                 {[...Array(5)].map((_, j) => (
                   <div
                     key={j}
-                    className="bg-gray-100 rounded p-2 shadow text-sm"
+                    className="bg-primary rounded p-2 shadow text-sm "
                   >
                     <p className="font-medium">Name: User {j + 1}</p>
                     <p>Phone: 91XXXXXXX{j}</p>
