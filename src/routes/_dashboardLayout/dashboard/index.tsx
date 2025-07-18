fd8d0e4bdde9ab5589d47b62fc24b5229974ccd2
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-07-18 15:52:53 +0530

    feat: pushed

--- src/routes/_dashboardLayout/dashboard/index.tsx
+++ src/routes/_dashboardLayout/dashboard/index.tsx
@@ -2,7 +2,7 @@ import LeadCard from "@/features/dashboard/components/LeadCard";
 import LeadTab from "@/features/dashboard/components/LeadTab";
 import type { LeadTabType } from "@/features/dashboard/components/LeadTab/LeadTab";
 // import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 
 import { createFileRoute } from "@tanstack/react-router";
