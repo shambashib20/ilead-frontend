ac54bba24eadf14fd3867b01dbd0a19845c2856b
shambashib20 <shambashibmajumdar2000@gmail.com>
2025-08-18 16:00:14 +0530

    feat: authLayout svg path adjusted

--- src/routes/_authLayout.tsx
+++ src/routes/_authLayout.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import Logo from "@/assets/logo.png";
+import loginAsset from "@/assets/login-v2.svg";
 
 export const Route = createFileRoute("/_authLayout")({
   beforeLoad: async ({ context }) => {
@@ -18,7 +19,7 @@ function AuthLayout() {
   return (
     <div className="auth_layout ">
       <div className=" items-center justify-center hidden lg:flex">
-        <img src="/public/login-v2.svg" alt="" className="w-[685px]" />
+        <img src={loginAsset} alt="" className="w-[685px]" />
       </div>
       <div className="px-10 pt-13 lg:pt-8 bg-primary">
         <img src={Logo} alt="" className="w-[160px] h-[55px]  block mx-auto" />
