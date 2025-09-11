410e5c91d552ce37b8d2222f40730da17694a792
Tushar Dutta <tz0540245@gmail.com>
2025-09-11 12:59:35 +0530

    new changes

--- src/features/leads/components/HeaderBtnModals/CreateLeadModal.tsx
+++ src/features/leads/components/HeaderBtnModals/CreateLeadModal.tsx
@@ -109,7 +109,7 @@ function CreateLeadModal() {
   };
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 h-[200px] overflow-y-scroll">
       <h2 className="text-lg font-semibold text-white">Create New Lead</h2>
 
       <div className="grid grid-cols-2 gap-4">
