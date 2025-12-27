abe8ffea0833edbf825f73e5b59b3765dfadcf16
Tushar Dutta <tz0540245@gmail.com>
2025-12-27 16:06:33 +0530

    new chnages

--- src/routes/_appLayout/index.tsx
+++ src/routes/_appLayout/index.tsx
@@ -1,12 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
-
 export const Route = createFileRoute("/_appLayout/")({
   component: RouteComponent,
-  beforeLoad: () => {
-    location.href = "/home";
-  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/_appLayout/"!</div>;
+  return (
+    <iframe
+      src="/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
 }
